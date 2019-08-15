478ce304c70559d056bd0107b78c12b85ee626e1
MohammadSS <mohammad.azimi.1996@gmail.com>
2019-08-15 15:34:25 +0000

    now site is up

---
+++ lib/models.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+from django.db import models
+import datetime
+from django.utils import timezone
+# for image fields
+from PIL import Image
+from django.urls import reverse 
+
+# All database and tables will be defined here . 
+
+
+# Categoury Model 
+# Each category will be one model that has Name ,
+# i.e : Category.name = Poems
+class Category(models.Model):
+    name = models.CharField(max_length=110)
+    def __unicode__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse("category", kwargs={"pk": self.pk})
+    
+# Books Model
+# each book will be one object of this model
+# if a book is reserved or out , backData MUST be filled 
+# when a book is reserved , backDate will be automatically set for 24 hours from now .
+class Book(models.Model):
+    name = models.CharField(max_length=110)
+    desc = models.CharField(max_length=400 , null = True , blank = True)
+    image = models.ImageField(upload_to='booksimage', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    documentNumber = models.CharField(max_length=15)
+    idNumber = models.CharField(max_length=400)
+    author = models.CharField(max_length=300)
+    publisher = models.CharField(max_length=100)
+    publishdate = models.CharField(max_length=50)
+    version = models.CharField(max_length=100 , null = True , blank = True)
+    lang = models.CharField(max_length=20 , null = True , blank = True)
+    info = models.CharField(max_length=400 , null = True , blank = True)
+    st = [('reserved' , 'Reserved') , ('available' , 'Available') , ('out' , 'out')]
+    status = models.CharField(max_length=10 , choices=st , default='available')
+    backDate = models.DateTimeField(blank=True , default=timezone.now)
+    def __unicode__(self):
+        return self.name
+    def image_url(self):
+        if self.image and hasattr(self.image, 'url'):
+            return self.image.url
---
+++ lib/views.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from datetime import datetime , timedelta
+# from django.utils.datetime_safe import new_datetime , timedelta
+from jalali_date import date2jalali , datetime2jalali
+from django.utils import timezone
+from django.shortcuts import render
+from django.http import HttpResponse , Http404 , HttpResponseRedirect
+import models
+from django.core.paginator import Paginator
+from django.urls import reverse 
+
+def index(request):
+    categorys = models.Category.objects.all()
+    context = {'cats' : categorys}
+    return render(request,"lib/index.html" , context)
+
+def category(request , pk):
+    category = models.Category.objects.get(pk=pk)
+    books = category.book_set.all()
+    paginator = Paginator(books, 10)
+    if request.method == 'GET' and 'page' in request.GET:
+        page = request.GET['page']
+        if page is not None and page != '':
+            current_page = page
+    else:
+        current_page = 1
+    books = paginator.page(current_page)
+    context = {'cat' : category , 'paginator' : paginator , 'books' : books , 'currentpage' : current_page}
+    return render(request,"lib/cats.html" , context)
+
+def books(request , pk):
+    
+    book = models.Book.objects.get(pk=pk)
+    if book.status!= "available":
+        difftime = book.backDate - timezone.now()  
+        print(datetime2jalali(book.backDate))
+        if ( difftime.days < 0):
+            book.status = "available"
+            book.backDate = timezone.now()
+            book.save()
+            book = models.Book.objects.get(pk=pk)
+    context = {'book' : book ,}
+    return render(request,"lib/book.html" , context)
+
+
+def reserveaction(request):
+    bookpk = request.POST['book']
+    book = models.Book.objects.get(pk=bookpk)
+    if book:
+        if book.status != "available":
+            resp = "book is not available"
+            err = 0 
+        else:
+            book.backDate = timezone.now() + timedelta(minutes = 5)
+            book.status = "reserved"
+            book.save()
+    book = models.Book.objects.get(pk=bookpk)
+    return HttpResponse(book.status)
