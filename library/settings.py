1fd24e8df44dfd730ae388ce87eca2f509c3d90c
Mohammad Azimi <Mohammad.azimi.1996@gmail.com>
2019-08-17 23:40:48 +0430

    setting for pythonanywhere.com

--- library/settings.py
+++ library/settings.py
@@ -149,13 +149,13 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-MEDIA_URL = '/media/'
-
-# MEDIA_ROOT = u'/home/MohammadSS/library/media'
-# MEDIA_URL = '/media/'
-# STATIC_ROOT = u'/home/MohammadSS/library/static'
 # STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+# MEDIA_URL = '/media/'
+
+MEDIA_ROOT = u'/home/MohammadSS/library/media'
+MEDIA_URL = '/media/'
+STATIC_ROOT = u'/home/MohammadSS/library/static'
+STATIC_URL = '/static/'
